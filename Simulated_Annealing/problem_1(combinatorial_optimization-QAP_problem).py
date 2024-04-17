a10618b3726353a7c724cf8a6bac861974ba8cd1
geun-hyoung <90395564+geun-hyoung@users.noreply.github.com>
2024-04-17 22:24:29 +0900

    <chore>SA Alogrithms add none improving moving

--- Simulated_Annealing/problem_1(combinatorial_optimization-QAP_problem).py
+++ Simulated_Annealing/problem_1(combinatorial_optimization-QAP_problem).py
@@ -1,6 +1,6 @@
-import math
 import numpy as np
 import random
+import math
 
 # 대각선을 기준으로 하반부는 흐름, 상반부는 거리
 raw_data = np.array([
@@ -38,15 +38,16 @@ def extract_distance_and_flow(matrix):
     flow += flow.T
     return distance, flow
 
-# 목적 함수
+# 목적 함수(양방향 흐름 모두 고려)
 def objective_function(solution):
     cost = 0
     for i in range(len(solution)):
         for j in range(len(solution)):
+            # 물리적 거리는 고정
             cost += flow[i][j] * distance[solution[i]][solution[j]]
     return cost
 
-# 이웃 정의
+# 이웃 정의 - 3D > 1D로 생각해서 두 자리를 변환
 def get_neighbor(solution):
     neighbor = solution.copy()
     i, j = random.sample(range(len(solution)), 2)
@@ -62,10 +63,13 @@ def simulated_annealing(temp, init_solution, cooling_rate, stop_condition, max_i
     while temp > stop_condition or max_iterations > iterations:
         new_solution = get_neighbor(current_solution)
         new_cost = objective_function(new_solution)
+        cost_difference = new_cost - current_cost
 
-        if new_cost < current_cost:
+        # Improving move + Non-improving move
+        if cost_difference < 0 or math.exp(-cost_difference / temp) > random.random():
             current_solution = new_solution
             current_cost = new_cost
+
         temp *= cooling_rate
         iterations += 1
 
@@ -86,57 +90,45 @@ for i in range(raw_data.shape[0]):
 distance += distance.T
 flow += flow.T
 
-# 1번 annealing schedule 2번 변경
-print('\n')
-stop_condition = 1
+# 초기 파라미터 설정
+temp = 10000
 init_solution = random.sample(range(15), 15)
+cooling_rate = 0.99
+stop_condition = 1
+iterations = 1000
 
-candidate = [
-    [100, 0.99],
-    [1000, 0.8],
-]
+# 1번 annealing schedule 2번 변경
+candidate = [[10000, 0.99], [10000, 0.8]]
 
 for obj in candidate:
-    temp, cooling_rate = obj[0], obj[1]
-    best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition)
-    print(f"temp: {temp}, cooling_rate = {cooling_rate}, Best solution: {best_solution}, Best cost: {best_cost}")
+    ch1_temp, ch1_cooling_rate = obj[0], obj[1]
+    best_solution, best_cost = simulated_annealing(ch1_temp, init_solution, ch1_cooling_rate, stop_condition, iterations)
+    print(f"temp: {ch1_temp}, cooling_rate = {ch1_cooling_rate}, Best solution: {best_solution}, Best cost: {best_cost/2}")
 
 # 2번 정지 조건 2번 바꾸기
 print('\n')
-temp = 100
-cooling_rate = 0.99
-init_solution = random.sample(range(15), 15)
-stop_condition = 1
+ch2_stop_condition = 1
 
-best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition)
-print(f"stop_condition: {stop_condition}, iterations = {1000}, Best solution: {best_solution}, Best cost: {best_cost}")
-
-iterations = 100
 best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition, iterations)
-print(f"stop_condition: {stop_condition}, iterations = {iterations}, Best solution: {best_solution}, Best cost: {best_cost}")
+print(f"stop_condition: {ch2_stop_condition}, iterations = {iterations}, Best solution: {best_solution}, Best cost: {best_cost/2}")
+
+ch2_stop_condition = 0.5
+best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition, iterations)
+print(f"stop_condition: {ch2_stop_condition}, iterations = {iterations}, Best solution: {best_solution}, Best cost: {best_cost/2}")
 
 # 3번 초기해 5번 바꾸면서 실험
 print('\n')
-temp = 100
-cooling_rate = 0.99
-stop_condition = 1
-
 for ord in range(5):
-    init_solution = random.sample(range(15), 15)
-    best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition)
-    print(f"change_order: {ord + 1}, Best solution: {best_solution}, Best cost: {best_cost}")
+    ch3_init_solution = random.sample(range(15), 15)
+    best_solution, best_cost = simulated_annealing(temp, ch3_init_solution, cooling_rate, stop_condition, iterations)
+    print(f"change_order: {ord + 1}, Best solution: {best_solution}, Best cost: {best_cost/2}")
 
 # 4번 랜덤 시드 10번
 print('\n')
-temp = 100
-cooling_rate = 0.99
-init_solution = random.sample(range(15), 15)
-stop_condition = 1
-
 for seed in range(10):
     random.seed(seed)
     # SA 실행
-    best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition)
-    print(f"Seed: {seed+1}, Best solution: {best_solution}, Best cost: {best_cost}")
+    best_solution, best_cost = simulated_annealing(temp, init_solution, cooling_rate, stop_condition, iterations)
+    print(f"Seed: {seed+1}, Best solution: {best_solution}, Best cost: {best_cost/2}")
 
-print("Problem 1 - SA Algorithms done")
+print("\nProblem 1 - SA Algorithms done")
--- Simulated_Annealing/problem_2(continuous_optimization).py
+++ Simulated_Annealing/problem_2(continuous_optimization).py
@@ -24,63 +24,55 @@ def simulated_annealing(initial_solution, temp=10000, cooling_rate=0.99, stoppin
         neighbor = get_neighbor(current_solution)
         neighbor_cost = object_function(neighbor[0], neighbor[1])
 
-        if neighbor_cost < current_cost:
+        cost_difference = neighbor_cost - current_cost
+
+        # Improving move + Non-improving move
+        if cost_difference < 0 or math.exp(-cost_difference / temp) > random.random():
             current_solution = neighbor
             current_cost = neighbor_cost
         temp *= cooling_rate
         iterations += 1
     return current_solution, current_cost
 
-# 1번 annealing schedule 2번 변경
-stopping_temp = 1
+# 초기 파라미터 설정
 init_solution = np.array([random.uniform(-3, 3), random.uniform(-2, 2)])
+temp = 100
+stopping_temp = 1
+cooling_rate = 0.99
+iterations = 1000
 
+# 1번 annealing schedule 2번 변경
 candidate = [
     [100, 0.99],
     [1000, 0.8],
 ]
-
 for obj in candidate:
-    temp, cooling_rate = obj[0], obj[1]
-    best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp)
+    ch1_temp, ch1_cooling_rate = obj[0], obj[1]
+    best_solution, best_cost = simulated_annealing(init_solution, ch1_temp, ch1_cooling_rate, stopping_temp, iterations)
     print(f"temp: {temp}, cooling_rate = {cooling_rate}, Best solution: {best_solution}, Best cost: {best_cost}")
 
 # 2번 정지 조건 2번 바꾸기
 print('\n')
-temp = 100
-cooling_rate = 0.99
-init_solution = np.array([random.uniform(-3, 3), random.uniform(-2, 2)])
-stopping_temp = 1
-
-best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp)
-print(f"stop_condition: {stopping_temp}, iterations = {1000}, Best solution: {best_solution}, Best cost: {best_cost}")
-
-iterations = 100
 best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp, iterations)
-print(f"stop_condition: {stopping_temp}, iterations = {iterations}, Best solution: {best_solution}, Best cost: {best_cost}")
+print(f"stop_condition: {stopping_temp}, Best solution: {best_solution}, Best cost: {best_cost}")
+
+ch2_stopping_temp = 0.5
+best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp, iterations)
+print(f"stop_condition: {ch2_stopping_temp}, Best solution: {best_solution}, Best cost: {best_cost}")
 
 # 3번 초기해 5번 바꾸면서 실험
 print('\n')
-temp = 100
-cooling_rate = 0.99
-stopping_temp = 1
-
 for ord in range(5):
-    init_solution = np.array([random.uniform(-3, 3), random.uniform(-2, 2)])
-    best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp)
+    ch3_init_solution = np.array([random.uniform(-3, 3), random.uniform(-2, 2)])
+    best_solution, best_cost = simulated_annealing(ch3_init_solution, temp, cooling_rate, stopping_temp, iterations)
     print(f"change_order: {ord + 1}, Best solution: {best_solution}, Best cost: {best_cost}")
 
 # 4번 랜덤 시드 10번
 print('\n')
-temp = 100
-cooling_rate = 0.99
-init_solution = np.array([random.uniform(-3, 3), random.uniform(-2, 2)])
-stopping_temp = 1
-
 for seed in range(10):
     random.seed(seed)
     # SA 실행
-    best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp)
+    best_solution, best_cost = simulated_annealing(init_solution, temp, cooling_rate, stopping_temp, iterations)
     print(f"Seed: {seed}, Best solution: {best_solution}, Best cost: {best_cost}")
 
-print("Problem 1 - SA Algorithms done")
+print("Problem 2 - SA Algorithms done")
